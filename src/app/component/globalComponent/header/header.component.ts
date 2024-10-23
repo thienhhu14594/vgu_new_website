92a9403307d48ec6db88c87d91d950ef222f3dc9
Thien <14594@student.vgu.edu.vn>
2024-10-23 15:23:24 +0700

    Update landing page

--- src/app/component/globalComponent/header/header.component.ts
+++ src/app/component/globalComponent/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { SubnavbarComponent } from '../subnavbar/subnavbar.component';
+import { NavbarComponentComponent } from "../navbar-component/navbar-component.component";
+import { LogoComponent } from "../logoComponent/logo/logo.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SubnavbarComponent, NavbarComponentComponent, LogoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/testframe/testframe.component.ts
+++ src/app/testframe/testframe.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Inject, PLATFORM_ID, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { TestComponent } from "../test/test.component";
 import { CommonModule } from '@angular/common';
 import { SubnavComponent } from "../component/globalComponent/subnav/subnav.component";
@@ -22,6 +22,7 @@ import { NavNewsComponent } from '../component/globalComponent/nav-news/nav-news
 import { NavigationEnd, Router } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 import { EventComponent } from '../component/globalComponent/event/event.component';
+import { HeaderComponent } from '../component/globalComponent/header/header.component';
 
 @Component({
   selector: 'app-testframe',
@@ -47,7 +48,8 @@ import { EventComponent } from '../component/globalComponent/event/event.compone
     PostsComponent,
     SlidePartnerComponent,
     NavNewsComponent,
-    EventComponent
+    EventComponent,
+    HeaderComponent
    ],
   templateUrl: './testframe.component.html',
   styleUrl: './testframe.component.css',
@@ -55,85 +57,9 @@ import { EventComponent } from '../component/globalComponent/event/event.compone
 })
 
 export class TestframeComponent implements AfterViewInit {
-  ////////////////////////
-//   constructor(private router: Router) { }
-//   ngOnInit() {
-//     this.router.events.subscribe((event) => {
-//         if (!(event instanceof NavigationEnd)) {
-//             return;
-//         }
-//         window.scrollTo(0, 0)
-//     });
-// }
-  checkCollapse() {
-    
-  }
-  currentPage: 'landing' | 'about' | 'study' | 'research' | 'industrial' = 'landing';
-
-  propertyleft: 'default' | 'onClick' = 'onClick';
-  propertyright: 'default' | 'onClick' = 'default';
-
-  propertyAbout: 'Default' | 'Hover' | 'Selected' = 'Default';
-  currentSelected: 'About' | 'Research' | 'Study' | 'Industrial' | 'none' = 'none';
-
-  propertyorangeBlue: 'Default' | 'Hover' = 'Default';
-  propertyorange: 'Default' | 'Hover' = 'Default';
-  propertywhite: 'Default' | 'Hover' = 'Default';
-  Methodleft() {
-    this.propertyleft = 'onClick';
-  }
-  Methodright(){
-    this.propertyright = 'onClick';
-  }
-  changeright(){
-    if (this.propertyright=='onClick')
-      this.propertyright = 'default'
-  }
-  changeleft(){
-    if (this.propertyleft=='onClick')
-      this.propertyleft = 'default'
-  }
-  setPropertyAbout(status: 'Default' | 'Hover' | 'Selected') {
-    if (status == 'Selected') {
-      this.currentSelected = 'About';
-    }
-    this.propertyAbout = status;
-    if (this.currentSelected == 'About') {
-      this.propertyAbout = 'Selected';
-    }
-  }
-  setDefault() {
-    //change selected menu base on actually current page
-    this.propertyAbout = 'Default';
-    switch(this.currentPage) {
-      case 'landing':
-        this.currentSelected = 'none'
-        break
-      case 'about':
-        this.currentSelected = 'About'
-        break
-      case 'study':
-        this.currentSelected = 'Study'
-        break
-      case 'research':
-        this.currentSelected = 'Research'
-        break
-      case 'industrial':
-        this.currentSelected = 'Industrial'
-        break
-    }
-  }
-
-  setPropertyorangeBlue(status: 'Default' | 'Hover') {
-    this.propertyorangeBlue = status;
-  }
-  setPropertyorange(status: 'Default' | 'Hover') {
-    this.propertyorange = status;
-  }
-  setPropertywhite(status: 'Default' | 'Hover') {
-    this.propertywhite = status;
-  }
 /////////////////////////////////////////////////////////////////
+//Lazy variables name N:news/A:admission/R:research/
+//U:upcoming event/P:university partner
   propertyN: 'Default' | 'Hover' | 'Selected' = 'Default';
   propertyA: 'Default' | 'Hover' | 'Selected' = 'Default';
   propertyR: 'Default' | 'Hover' | 'Selected' = 'Default';
@@ -150,43 +76,76 @@ export class TestframeComponent implements AfterViewInit {
 
   setPropertyN(status: 'Default' | 'Hover' | 'Selected') {
     if (this.propertyN == 'Selected') {return;}
-    this.setNavNewsDefault();
     this.propertyN = status;
   }
-  setPropertyA() {
-
+  setPropertyA(status: 'Default' | 'Hover' | 'Selected') {
+    if (this.propertyA == 'Selected') {return;}
+    this.propertyA = status;
   }
-  setPropertyR() {
-
+  setPropertyR(status: 'Default' | 'Hover' | 'Selected') {
+    if (this.propertyR == 'Selected') {return;}
+    this.propertyR = status;
   }
-  setPropertyU() {
-
+  setPropertyU(status: 'Default' | 'Hover' | 'Selected') {
+    if (this.propertyU == 'Selected') {return;}
+    this.propertyU = status;
   }
-  setPropertyP() {
-
+  setPropertyP(status: 'Default' | 'Hover' | 'Selected') {
+    if (this.propertyP == 'Selected') {return;}
+    this.propertyP = status;
   }
 
-  constructor(private cdr: ChangeDetectorRef) { }
-  @ViewChildren('st2, st3, st4, st5, stl') elements!: QueryList<ElementRef>; // Reference to all elements
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: any,
+    private cdr: ChangeDetectorRef
+  ) { }
+  @ViewChildren('intro, news, admission, st4, st5, stl') elements!: QueryList<ElementRef>; // Reference to all elements
 
   ngAfterViewInit() {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          if (entry.target.className == 'st2') {
-            console.log(`Scrolled to st2`);
-            this.propertyN = 'Selected';
-            console.log(`propertyN = ${this.propertyN}`);
+    if (isPlatformBrowser(this.platformId) && 'IntersectionObserver' in window) {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            console.log(`${entry.target.className}`);
+            this.setNavNewsDefault();
+            switch (entry.target.className) {
+              case 'news':
+                this.propertyN = 'Selected';
+                break;
+              case 'admission':
+                this.propertyA = 'Selected';
+                break;
+              case 'st4':
+                this.propertyR = 'Selected';
+                break;
+              case 'st5':
+                this.propertyU = 'Selected';
+                break;
+              case 'stl':
+                this.propertyP = 'Selected';
+                break;
+              default: this.setNavNewsDefault(); break;
+            }
             this.cdr.detectChanges();
           }
-        }
-      });
-    });
+          else {
+            // this.setNavNewsDefault();
+          }
+        });
+      }, {
+        root: null,
+        rootMargin: '0px',
+        threshold: [0.33, 0.5, 0.75, 1]
+      }
+    );
 
-    // Start observing all elements
-    this.elements.forEach((el) => {
-      observer.observe(el.nativeElement);
-    });
+      // Start observing all elements
+      this.elements.forEach((el) => {
+        observer.observe(el.nativeElement);
+      });
+  } else {
+    console.error('IntersectionObserver not supported or not in browser');
+  }
   }
 
   scroll(el: HTMLElement) {
