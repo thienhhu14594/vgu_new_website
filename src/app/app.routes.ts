5d6b3704bd4157285a8bc0586c0e97d1d6003e40
Thien <14594@student.vgu.edu.vn>
2024-11-06 10:04:17 +0700

    finish landing page for pc and mobile responsive

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,9 +6,11 @@ import { PostComponent } from './component/post/post.component';
 import { TestComponent } from './test/test.component';
 import { TestframeComponent } from './testframe/testframe.component';
 import { LandingPageDesktopComponent } from './component/landing-page-desktop/landing-page-desktop.component';
+import { LandingPageMobileComponent } from './component/landing-page-mobile/landing-page-mobile.component';
 
 export const routes: Routes = [
   {path: 'landing-desktop', component: LandingPageDesktopComponent},
+  {path: 'landing-mobile', component: LandingPageMobileComponent},
   // {path: '', component: GlobalComponent},
   {path: 'blog', component: PostsComponent},
   // {path: ':slug', component: PageComponent},
