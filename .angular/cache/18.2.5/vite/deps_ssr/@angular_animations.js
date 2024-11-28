769a92fe9e00ecc62f5cd581debc010d4d2a5aa6
DatLitt <141332767+DatLitt@users.noreply.github.com>
2024-11-28 13:51:48 +0700

    2811

---
+++ .angular/cache/18.2.5/vite/deps_ssr/@angular_animations.js
@@ -0,0 +1,52 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationGroupPlayer,
+  AnimationMetadataType,
+  BrowserAnimationBuilder,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  ɵPRE_STYLE
+} from "./chunk-V4IJ7RPU.js";
+import "./chunk-3WPEY6LC.js";
+import "./chunk-POE4UYHY.js";
+import "./chunk-AOODNVNQ.js";
+import "./chunk-MV52JWQN.js";
+export {
+  AUTO_STYLE,
+  AnimationBuilder,
+  AnimationFactory,
+  AnimationMetadataType,
+  NoopAnimationPlayer,
+  animate,
+  animateChild,
+  animation,
+  group,
+  keyframes,
+  query,
+  sequence,
+  stagger,
+  state,
+  style,
+  transition,
+  trigger,
+  useAnimation,
+  AnimationGroupPlayer as ɵAnimationGroupPlayer,
+  BrowserAnimationBuilder as ɵBrowserAnimationBuilder,
+  ɵPRE_STYLE
+};
+//# sourceMappingURL=@angular_animations.js.map
