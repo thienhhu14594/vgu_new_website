523b4f377cd4ce4d92f013b25495d7c54a87e627
DatLitt <141332767+DatLitt@users.noreply.github.com>
2024-12-06 16:12:46 +0700

    update color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
 import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './global-components/footer/footer.component';
+import { HeaderComponent } from './global-components/header/header.component';
+import { DirectusService } from '../../directus.service';
 
 @Component({
   selector: 'app-root',
@@ -26,5 +27,20 @@ export class AppComponent {
     const scrollPosition = (event.target as HTMLElement).scrollTop;
     console.log('Content scrolled:', scrollPosition);
     this.isScrolled = scrollPosition > 120;
+  }
+
+  //color
+  constructor(private colorService: DirectusService) {}
+
+  ngOnInit() {
+    this.colorService.getThemeColors().subscribe((data) => {
+      this.setCSSVariable('primary-color', data.data.primaryColor.color);
+      console.log(data.data.primaryColor.color);
+    });
+  }
+
+  setCSSVariable(variableName: string, color: string) {
+    // Set the dynamic value for the CSS variable on the root element
+    document.documentElement.style.setProperty(`--${variableName}`, color);
   }
 }
