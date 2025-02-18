71df9ef7ebd46fb3c77a4181036df0bb6aa061fd
DatLitt <141332767+DatLitt@users.noreply.github.com>
2025-02-18 20:27:21 +0700

    udate pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { OrganizationComponent } from './pages/organization/organization.compone
 
 import { provideRouter, RouterModule, Routes } from '@angular/router';
 import { StudyProgramsComponent } from './pages/study-programs/study-programs.component';
+import { ProgramDetailComponent } from './pages/program-detail/program-detail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/landing-page', pathMatch: 'full' },
   { path: 'landing-page', component: LandingPageComponent },
   { path: 'organization', component: OrganizationComponent },
   { path: 'programs', component: StudyProgramsComponent },
+  { path: 'program-detail', component: ProgramDetailComponent },
 ];
--- src/app/pages/study-programs/study-programs.component.ts
+++ src/app/pages/study-programs/study-programs.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { PrimaryButtonComponent } from '../../global-components/buttons/primary-button/primary-button.component';
+import { Card3MComponent } from '../landing-page/components/card3-m/card3-m.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-study-programs',
   standalone: true,
-  imports: [],
+  imports: [PrimaryButtonComponent, Card3MComponent, CommonModule],
   templateUrl: './study-programs.component.html',
-  styleUrl: './study-programs.component.css'
+  styleUrl: './study-programs.component.css',
 })
 export class StudyProgramsComponent {
-
+  tests: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
