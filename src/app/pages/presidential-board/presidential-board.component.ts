88481c70ccac7895cdbe40b3ed32aace79dae234
DatLitt <141332767+DatLitt@users.noreply.github.com>
2025-02-27 11:05:32 +0700

    update

---
+++ src/app/pages/presidential-board/presidential-board.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presidential-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './presidential-board.component.html',
+  styleUrl: './presidential-board.component.css'
+})
+export class PresidentialBoardComponent {
+
+}
