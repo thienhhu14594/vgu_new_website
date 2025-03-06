bb77a388557b773945f3ebf27c31b03b41eb7ec0
DatLitt <141332767+DatLitt@users.noreply.github.com>
2025-03-06 14:22:30 +0700

    update apiurl

--- dist/vgu_new_website/server/main.server.mjs
+++ dist/vgu_new_website/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MPDYYK6W.mjs";import"./chunk-ZQ6IRS3E.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-3JIJC25L.mjs";import"./chunk-ZQ6IRS3E.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
