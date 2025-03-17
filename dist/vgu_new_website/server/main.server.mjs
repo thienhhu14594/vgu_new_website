7e38af35534e8206dbce8d37bee98d08a91d4b7e
Thien <14594@student.vgu.edu.vn>
2025-03-17 17:09:47 +0700

    update organization

--- dist/vgu_new_website/server/main.server.mjs
+++ dist/vgu_new_website/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-SAVOGY4M.mjs";import"./chunk-653GVTDQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-QKCZ35NO.mjs";import"./chunk-653GVTDQ.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
